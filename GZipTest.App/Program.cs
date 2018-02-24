33fc3ddb830f0b974d49061fa1db41019bbd7e05
malast <malast88@gmail.com>
2018-02-24 19:21:54 +0100

    added exception catch on assembly

--- GZipTest.App/Program.cs
+++ GZipTest.App/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace GZipTest.App
+﻿using System;
+
+namespace GZipTest.App
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var inputFile = args[0];
+                var outputFile = args[1];
+                System.IO.File.WriteAllText(outputFile, "test");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: '{ex.Message}'");
+            }
         }
     }
 }
--- GZipTest.RegressionTests/ExeTest.cs
+++ GZipTest.RegressionTests/ExeTest.cs
@@ -12,21 +12,71 @@ namespace GZipTest.RegressionTests
         public void ExeShouldCompressAndDecompress()
         {
             // Arrange
+            var exePath = GetExePath();
             var assemblyPath = Assembly.GetExecutingAssembly().Location;
-            var exePath = Path.Combine(assemblyPath, @"..\..\..\..\GZipTest.App\bin\Debug\GZipTest.exe");
-            var inputFile = Path.Combine(assemblyPath, @"Data\1.txt");
-            var compressedFile = Path.Combine(assemblyPath, @"Data\1.gz");
-            var decompressedFile = Path.Combine(assemblyPath, @"Data\1_.txt");
+            var assembyDirectory = Path.GetDirectoryName(assemblyPath);
+            var inputFile = Path.Combine(assembyDirectory, @"Data\1.txt");
+            var compressedFile = Path.Combine(assembyDirectory, @"Data\1.gz");
+            var decompressedFile = Path.Combine(assembyDirectory, @"Data\1_.txt");
 
             // Act
-            Process.Start(exePath, $"\"{inputFile}\" + \"{compressedFile}\"").WaitForExit();
-            Process.Start(exePath, $"\"{compressedFile}\" + \"{decompressedFile}\"").WaitForExit();
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = exePath,
+                Arguments = $"\"{inputFile}\" \"{compressedFile}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            }).WaitForExit();
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = exePath,
+                Arguments = $"\"{compressedFile}\" \"{decompressedFile}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            }).WaitForExit();
 
             // Assert
             var compressedBytes = File.ReadAllBytes(compressedFile);
             Assert.AreEqual(0x1f, compressedBytes[0]);
             Assert.AreEqual(0x8b, compressedBytes[1]);
             Assert.AreEqual(File.ReadAllBytes(inputFile), File.ReadAllBytes(decompressedFile));
+        }
+
+        [Test]
+        public void ExeShouldCatchAnError()
+        {
+            // Arrange
+            var exePath = GetExePath();
+            
+            // Act
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = exePath,
+                    Arguments = string.Empty,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            proc.Start();
+
+            // Assert
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine();
+                Assert.IsTrue(line.StartsWith("Unexpected error"));
+            }
+        }
+
+        private string GetExePath()
+        {
+            var assemblyPath = Assembly.GetExecutingAssembly().Location;
+            var assembyDirectory = Path.GetDirectoryName(assemblyPath);
+            return Path.Combine(assembyDirectory, @"..\..\..\GZipTest.App\bin\Debug\GZipTest.exe");
         }
     }
 }
